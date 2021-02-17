ca1b429a574615a27c2c87d29684af8d9ef81415
geeend <38340742+GeeEnd@users.noreply.github.com>
2021-02-17 09:56:16 +0800

    Add files via upload

---
+++ string-passing-to-a-function.c
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+void displayString(char str[]);
+
+int main()
+{
+    char str[50];
+    printf("Enter string: ");
+    fgets(str, sizeof(str), stdin);             
+    displayString(str);     // Passing string to a function.    
+    return 0;
+}
+void displayString(char str[])
+{
+    printf("String Output: ");
+    puts(str);
+}
